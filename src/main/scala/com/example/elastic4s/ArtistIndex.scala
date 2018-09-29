e56b47f5af9938b19f9dfdd45a2e0c08ab29b219
PJ Fanning <fanningpj@yahoo.com>
2018-09-29 16:25:24 +0100

    init

---
+++ src/main/scala/com/example/elastic4s/ArtistIndex.scala
@@ -0,0 +1,68 @@
+import com.sksamuel.elastic4s.RefreshPolicy
+import com.sksamuel.elastic4s.embedded.LocalNode
+import com.sksamuel.elastic4s.http._
+import com.sksamuel.elastic4s.http.search.SearchResponse
+
+object ArtistIndex extends App {
+
+  // spawn an embedded node for testing
+  //val localNode = LocalNode("mycluster", "/tmp/datapath")
+
+  // in this example we create a client attached to the embedded node, but
+  // in a real application you would provide the HTTP address to the ElasticClient constructor.
+  //val client = localNode.client(shutdownNodeOnClose = true)
+
+  val client = ElasticClient(ElasticProperties("http://localhost:9200"))
+
+  // we must import the dsl
+  import com.sksamuel.elastic4s.http.ElasticDsl._
+
+  // Next we create an index in advance ready to receive documents.
+  // await is a helper method to make this operation synchronous instead of async
+  // You would normally avoid doing this in a real program as it will block
+  // the calling thread but is useful when testing
+
+  //client.execute {
+  //  deleteIndex("artists")
+  //}.await
+
+  client.execute {
+    createIndex("artists").mappings(
+      mapping("modern").fields(
+        textField("name")
+      )
+    )
+  }.await
+
+  // Next we index a single document which is just the name of an Artist.
+  // The RefreshPolicy.Immediate means that we want this document to flush to the disk immediately.
+  // see the section on Eventual Consistency.
+  client.execute {
+    indexInto("artists" / "modern").id("L.S. Lowry").fields("birthPlace" -> "Manchester")
+      .refresh(RefreshPolicy.Immediate)
+  }.await
+  client.execute {
+    indexInto("artists" / "modern").id("Georges Seurat").fields("birthPlace" -> "Paris")
+      .refresh(RefreshPolicy.Immediate)
+  }.await
+
+  // now we can search for the document we just indexed
+  val resp = client.execute {
+    search("artists") query "Manchester"
+  }.await
+
+  // resp is a Response[+U] ADT consisting of either a RequestFailure containing the
+  // Elasticsearch error details, or a RequestSuccess[U] that depends on the type of request.
+  // In this case it is a RequestSuccess[SearchResponse]
+
+  println("---- Search Results ----")
+  resp match {
+    case failure: RequestFailure => println("We failed " + failure.error)
+    case results: RequestSuccess[SearchResponse] => println(results.result.hits.hits.toList)
+  }
+
+  // Response also supports familiar combinators like map / flatMap / foreach:
+  resp foreach (search => println(s"There were ${search.totalHits} total hits"))
+
+  client.close()
+}
